6ea847188216fc4d2d335b53e93bb36004e0e0f4
Elliot Koh <109998783+ProximoBinks@users.noreply.github.com>
2025-02-11 19:16:48 +1030

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -17,10 +17,11 @@ export default function Services() {
                     <div className="bg-white shadow-lg rounded-lg p-6 border border-gray-200">
                         <div className="flex items-center space-x-4 mb-4">
                             <FaStethoscope className="text-red-600 text-4xl" />
-                            <h2 className="text-2xl font-semibold text-gray-800">Occupational Physician</h2>
+                            <h2 className="text-2xl font-semibold text-gray-800">Specialised Services</h2>
                         </div>
-                        <h3 className="text-lg font-medium text-gray-700 mb-2">Specialised Services</h3>
+                        {/* <h3 className="text-lg font-medium text-gray-700 mb-2">Specialised Services</h3> */}
                         <ul className="list-disc pl-6 space-y-2 text-black mb-4 font-bold">
+                            <li>Occupational Physician</li>
                             <li>Psychiatry</li>
                             <li>Psychology</li>
                             <li>Injury Management</li>
