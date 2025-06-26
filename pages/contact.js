ccfc4157ae0a74e362912ef71c2f780870582fe7
Elliot Koh <109998783+ProximoBinks@users.noreply.github.com>
2025-06-26 13:02:16 +0930

    Updated contact page info + package lock info

--- pages/contact.js
+++ pages/contact.js
@@ -78,56 +78,15 @@ export default function Contact() {
           <span className="text-gray-900 font-medium"> 08 8311 1755</span>
         </p>
 
-        {/* St Morris Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12 items-start">
-          <div className="border border-gray-300 p-6 rounded-lg">
-            <h3 className="text-xl font-semibold mb-4">Specialist Plus - St Morris</h3>
-            <p>1 A Williams Avenue, St Morris, SA 5068.</p>
-            <p className="mt-4 font-semibold">ST MORRIS WORKING HOURS:</p>
-            <p>Monday: 7.30am - 3.30pm</p>
-            <p>Tuesday to Friday: 7.30am - 4.30pm</p>
-            <div className="mt-4">
-              <p className="flex items-center space-x-2">
-                <FaPhone className="text-red-600" />
-                <a href="tel:+61884236477">(08) 8423 6477</a>
-              </p>
-              <p className="flex items-center space-x-2 mt-2">
-                <FaEnvelope className="text-red-600" />
-                <a href="mailto:admin@specialistplus.com.au">admin@specialistplus.com.au</a>
-              </p>
-            </div>
-            <div className="mt-6">
-              <a
-                href="https://www.google.com/maps/dir//Specialist+Plus+-+St+Morris/data=!4m6!4m5!1m0!1m2!1m1!1s0x6ab0cb7939e68915:0xf78c5ddd0d188532!2m2!1d138.65407417729955!2d-34.91342997284493"
-                target="_blank"
-                className="text-blue-500 hover:text-blue-800 flex items-center space-x-2 transition-all"
-              >
-                <FaMapMarkerAlt />
-                <span>Driving Directions and Location Map</span>
-              </a>
-            </div>
-          </div>
-          <div className="w-full h-full rounded-lg shadow-lg overflow-hidden">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.721817731735!2d138.65407417729955!3d-34.91342997284493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ab0cb7939e68915%3A0xf78c5ddd0d188532!2sSpecialist%20Plus%20-%20St%20Morris!5e0!3m2!1sen!2sau!4v1727157327429!5m2!1sen!2sau"
-              width="100%"
-              height="400"
-              allowFullScreen=""
-              loading="eager"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="w-full h-full"
-            ></iframe>
-          </div>
-        </div>
-
         {/* Richmond Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12 items-start">
           <div className="border border-gray-300 p-6 rounded-lg">
             <h3 className="text-xl font-semibold mb-4">Specialist Plus - Richmond</h3>
             <p>129 Marion Road, Richmond, SA 5033.</p>
             <p className="mt-4 font-semibold">RICHMOND WORKING HOURS:</p>
-            <p>Tuesday and Thursday: 8.30am - 5.00pm</p>
-            <p>Wednesday and Friday: 8.30am - 4.30pm</p>
+            <p>Monday: 8.00am - 4.00pm</p>
+            <p>Tuesday to Thursday: 8.00am - 5.00pm</p>
+            <p>Friday: 8.00am - 4.30pm</p>
             <div className="mt-4">
               <p className="flex items-center space-x-2">
                 <FaPhone className="text-red-600" />
@@ -178,7 +137,7 @@ export default function Contact() {
 
             <p className="hidden">
               <label>
-                Don’t fill this out if you're human: <input name="bot-field" />
+                Don't fill this out if you're human: <input name="bot-field" />
               </label>
             </p>
 
